ed6d67130306727c210f0766552e360f32fa6a02
Jona Maiss <maissjona2000@gmail.com>
2019-05-02 15:09:44 +0200

    Vehicle Browser Jona

--- vehicle-model-browser/src/app/model/model.component.ts
+++ vehicle-model-browser/src/app/model/model.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
-export interface ICar{
+export interface ICar {
   id: number;
   year: number;
   make: string;
@@ -17,10 +17,14 @@ export interface ICar{
 export class ModelComponent implements OnInit {
 
   private host: string = 'https://vehicle-data.azurewebsites.net';
+  stillModels: boolean = true;
+  modelZero: boolean = false;
   years: number[];
   makes: string[];
   yearChosen: number = 0;
   makeChosen: string = '';
+  offset: number = 0;
+  fetch: number;
   filterMakes: string[];
   models: ICar[];
 
@@ -31,24 +35,59 @@ export class ModelComponent implements OnInit {
     this.getCars();
   }
 
-  async getYearsMakes(){
+  async getYearsMakes() {
     this.years = await this.http.get<number[]>(`${this.host}\\api\\years`).toPromise();
     this.makes = await this.http.get<string[]>(`${this.host}\\api\\makes`).toPromise();
   }
 
-  async getCars(){
-    if(this.makeChosen !== ''){
+  async getCars() {
+    if (this.makeChosen !== '') {
       this.models = await this.http.get<ICar[]>(`${this.host}\\api\\models?make=${this.makeChosen}`).toPromise();
     }
-    if(this.yearChosen !== 0 && this.makeChosen === ''){
+    if (this.yearChosen !== 0 && this.makeChosen === '') {
       this.models = await this.http.get<ICar[]>(`${this.host}\\api\\models?year=${this.yearChosen}`).toPromise();
     }
-    if(this.yearChosen !== 0 && this.makeChosen !== ''){
+    if (this.yearChosen !== 0 && this.makeChosen !== '') {
       this.models = await this.http.get<ICar[]>(`${this.host}\\api\\models?make=${this.makeChosen}&year=${this.yearChosen}`).toPromise();
     }
-    if(this.makeChosen === '' && this.yearChosen === 0){
+    if (this.makeChosen === '' && this.yearChosen === 0) {
       this.models = await this.http.get<ICar[]>(`${this.host}\\api\\models`).toPromise();
     }
   }
 
+  async previousPage() {
+    this.offset -= 10;
+    if (this.makeChosen !== '') {
+      this.models = await this.http.get<ICar[]>(`${this.host}\\api\\models?make=${this.makeChosen}&offset=${this.offset}`).toPromise();
+    }
+    if (this.yearChosen !== 0) {
+      this.models = await this.http.get<ICar[]>(`${this.host}\\api\\models?year=${this.yearChosen}&offset=${this.offset}`).toPromise();
+    }
+    if (this.yearChosen !== 0 && this.makeChosen !== '') {
+      this.models = await this.http.get<ICar[]>(`${this.host}\\api\\models?make=${this.makeChosen}&year=${this.yearChosen}&offset=${this.offset}`).toPromise();
+    } else {
+      this.models = await this.http.get<ICar[]>(`${this.host}\\api\\models?offset=${this.offset}`).toPromise();
+    }
+    if (this.offset >= 10) {
+      this.modelZero = true;
+    }
+  }
+
+  async nextPage() {
+    this.offset += 10;
+    if (this.makeChosen !== '') {
+      this.models = await this.http.get<ICar[]>(`${this.host}\\api\\models?make=${this.makeChosen}&offset=${this.offset}`).toPromise();
+    }
+    if (this.yearChosen !== 0) {
+      this.models = await this.http.get<ICar[]>(`${this.host}\\api\\models?year=${this.yearChosen}&offset=${this.offset}`).toPromise();
+    }
+    if (this.yearChosen !== 0 && this.makeChosen !== '') {
+      this.models = await this.http.get<ICar[]>(`${this.host}\\api\\models?make=${this.makeChosen}&year=${this.yearChosen}&offset=${this.offset}`).toPromise();
+    } else {
+      this.models = await this.http.get<ICar[]>(`${this.host}\\api\\models?offset=${this.offset}`).toPromise();
+    }
+    if (this.offset <= this.models.length) {
+      this.stillModels = false;
+    }
+  }
 }
